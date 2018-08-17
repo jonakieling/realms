401edab4811bb2be7c4ef18afe85f4aa71b7ecd6
Jonathan Kieling <code@j0na.net>
2018-08-17 23:38:18 +0200

    server response function and client list

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 
+use std::net::TcpStream;
 use std::net::TcpListener;
 use std::net::Shutdown;
 use std::io::prelude::*;
@@ -24,7 +25,7 @@ pub struct Universe {
 
 impl Universe {
 	pub fn run(mut self, t: &mut Terminal<RawBackend>) -> Result<(), io::Error> {
-	    draw_dashboard(t, &self.requests)?;
+	    draw_dashboard(t, &self.requests, &self.clients)?;
 	    for stream in self.listener.incoming() {
 			let mut stream = stream.unwrap();
 			loop {
@@ -34,7 +35,7 @@ impl Universe {
 			    stream.flush().unwrap();
 
 
-			    let request: RealmsProtocol = deserialize(&buffer).expect("could not deserialize client request");
+			    let request: RealmsProtocol = deserialize(&buffer).expect("could not deserialize client request.");
 
 			    match request {
 			        RealmsProtocol::CONNECT(_) => {
@@ -42,10 +43,8 @@ impl Universe {
 			        	if let Some(current_highest) = self.clients.iter().max() {
 			        	    new_id = (current_highest + 1).max(new_id);
 			        	}
-			    		let data = serialize(&RealmsProtocol::CONNECT(Some(new_id))).expect("could not serialize data package for connect response.");
-						stream.write(&data).expect("could not write to tcp stream.");
+						send_response(&RealmsProtocol::CONNECT(Some(new_id)), &stream)?;
 						self.clients.push(new_id);
-						stream.flush().unwrap();
 			    		self.requests.push(request);
 			        },
 			        RealmsProtocol::REALM(_) => {
@@ -56,45 +55,35 @@ impl Universe {
 			    			id,
 			    			age: 0
 			    		};
-			    		let realm_bytes = serialize(&realm).expect("could not serialize realm");
-			    		let data = serialize(&RealmsProtocol::REALM(Some(realm_bytes))).expect("could not serialize data package for realm response.");
-						stream.write(&data).expect("could not write to tcp stream.");
+			    		let realm_bytes = serialize(&realm).expect("could not serialize realm.");
+						send_response(&RealmsProtocol::REALM(Some(realm_bytes)), &stream)?;
 			    		self.realms.push(realm);
-						stream.flush().unwrap();
 			    		self.requests.push(request);
 			        },
 			        RealmsProtocol::ISLAND(_) => {
-			    		let island_bytes = serialize(&Island::new()).expect("could not serialize island");
-			    		let data = serialize(&RealmsProtocol::ISLAND(Some(island_bytes))).expect("could not serialize data package for island response.");
-						stream.write(&data).expect("could not write to tcp stream.");
-						stream.flush().unwrap();
+			    		let island_bytes = serialize(&Island::new()).expect("could not serialize island.");
+						send_response(&RealmsProtocol::ISLAND(Some(island_bytes)), &stream)?;
 			    		self.requests.push(request);
 			        },
 			        RealmsProtocol::EXPEDITION(_) => {
-			    		let expedition_bytes = serialize(&Expedition::new()).expect("could not serialize expedition");
-			    		let data = serialize(&RealmsProtocol::EXPEDITION(Some(expedition_bytes))).expect("could not serialize data package for expedition response.");
-						stream.write(&data).expect("could not write to tcp stream.");
-						stream.flush().unwrap();
+			    		let expedition_bytes = serialize(&Expedition::new()).expect("could not serialize expedition.");
+						send_response(&RealmsProtocol::EXPEDITION(Some(expedition_bytes)), &stream)?;
 			    		self.requests.push(request);
 			        },
 			        RealmsProtocol::STATE(_) => {
-			    		let state_bytes = serialize(&state()).expect("could not serialize state");
-			    		let data = serialize(&RealmsProtocol::STATE(Some(state_bytes))).expect("could not serialize data package for state response.");
-						stream.write(&data).expect("could not write to tcp stream.");
-						stream.flush().unwrap();
+			    		let state_bytes = serialize(&state()).expect("could not serialize state.");
+						send_response(&RealmsProtocol::STATE(Some(state_bytes)), &stream)?;
 			        },
 			        RealmsProtocol::QUIT => {
-			    		let data = serialize(&RealmsProtocol::QUIT).expect("could not serialize data package for quit response.");
-						stream.write(&data).expect("could not write to tcp stream.");
-						stream.flush().unwrap();
-						stream.shutdown(Shutdown::Both).expect("connection should have terminated.");
+						send_response(&RealmsProtocol::QUIT, &stream)?;
+						stream.shutdown(Shutdown::Both).expect("stream could not shut down.");
 			    		self.requests.push(request);
-			    		// draw dashboard update befor client exits
-			    		draw_dashboard(t, &self.requests)?;
+			    		// draw dashboard update before client exits
+			    		draw_dashboard(t, &self.requests, &self.clients)?;
 			        	break;
 			        }
 			    }
-			    draw_dashboard(t, &self.requests)?;
+			    draw_dashboard(t, &self.requests, &self.clients)?;
 			}
 	    }
 	    t.show_cursor().unwrap();
@@ -104,25 +93,42 @@ impl Universe {
 	}
 }
 
+fn send_response(data: &RealmsProtocol, mut stream: &TcpStream) -> Result<(), io::Error> {
+	let raw = serialize(data).expect("could not serialize data for response.");
+	stream.write(&raw).expect("could not write to tcp stream.");
+	stream.flush().unwrap();
+	Ok(())
+}
 
-fn draw_dashboard(t: &mut Terminal<RawBackend>, requests: &Vec<RealmsProtocol>) -> Result<(), io::Error> {
+
+fn draw_dashboard(t: &mut Terminal<RawBackend>, requests: &Vec<RealmsProtocol>, clients: &Vec<usize>) -> Result<(), io::Error> {
 	let t_size = t.size().unwrap();
 
 	Group::default()
-        .direction(Direction::Vertical)
-		.sizes(&[Size::Min(0)])
+        .direction(Direction::Horizontal)
+		.sizes(&[Size::Percent(50), Size::Percent(50)])
         .render(t, &t_size, |t, chunks| {
             let style = Style::default();
-        	let requests = requests.iter().rev().map(|ref mut request| {
-                    Item::StyledData(
-                        format!("{}", request),
-                        &style
-                    )
-                });
+        	let requests = requests.iter().rev().map(|request| {
+                Item::StyledData(
+                    format!("{}", request),
+                    &style
+                )
+            });
+        	let clients = clients.iter().rev().map(|client| {
+                Item::StyledData(
+                    format!("{}", client),
+                    &style
+                )
+            });
 
     		List::new(requests)
-                .block(Block::default().borders(Borders::ALL).title("Realms Server"))
+                .block(Block::default().borders(Borders::ALL).title("Realms Requests"))
                 .render(t, &chunks[0]);
+
+    		List::new(clients)
+                .block(Block::default().borders(Borders::ALL).title("Realms Clients"))
+                .render(t, &chunks[1]);
         });
 	t.draw()
 }
